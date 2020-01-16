e4735b0e814d2aa04495575e26d0c5ccdab0a0be
Pradeep Kolli <pradeepkolli3@yahoo.in>
2020-01-16 00:00:34 -0700

    added redux logger to get the state of the applicaition and actions performed to be logged in the console.

--- src/Components/IceCreamContainer.js
+++ src/Components/IceCreamContainer.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 function IceCreamContainer(props) {
     return (
         <div>
-            <h2>Number of cakes - {props.numOfIceCreams} </h2>
+            <h2>Number of Icecreams - {props.numOfIceCreams} </h2>
             <button onClick={props.buyIceCream}>BUY ICECREAM</button>
         </div>
     )
